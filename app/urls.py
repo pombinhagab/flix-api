e023d929685346f99c1cfe2e062393162a4f538c
Gabriel N. <gabrielnascimento12t@gmail.com>
2026-02-19 23:50:20 -0300

    new app movies

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetriveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls), # django admin
     
-    path('genres/', GenreCreateListView.as_view(), name='genres-create-list'), # all genres
-    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view') # one specific genre
+    
+    path('genres/', GenreCreateListView.as_view(), name='genres-create-list'), 
+    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view'),
+     
+
+    path('actors/', ActorCreateListView.as_view(), name='actors-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actors-detail-view'),
+
+
+    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>/', MovieRetriveUpdateDestroyView.as_view(), name='movie-detail-view'),
 ]

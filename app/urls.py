7fa2329df9804acb8ca13958bf3133ebabe7fae1
Gabriel N. <gabrielnascimento12t@gmail.com>
2026-02-23 16:24:30 -0300

    versioning urls and adding rate to movies

--- app/urls.py
+++ app/urls.py
@@ -1,26 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetriveUpdateDestroyView
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls), # django admin
-    
-    
-    path('genres/', GenreCreateListView.as_view(), name='genres-create-list'), 
-    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view'),
-     
+    path('admin/', admin.site.urls),
 
-    path('actors/', ActorCreateListView.as_view(), name='actors-create-list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actors-detail-view'),
-
-
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>/', MovieRetriveUpdateDestroyView.as_view(), name='movie-detail-view'),
-
-
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
+    path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 ]
